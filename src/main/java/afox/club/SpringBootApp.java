e0daf2302b92c8f2b72dd68f28c3700e2fba929d
ruhandb <ruhandb@gmail.com>
2016-11-15 10:51:43 -0200

    pages

--- src/main/java/afox/club/SpringBootApp.java
+++ src/main/java/afox/club/SpringBootApp.java
@@ -2,17 +2,13 @@ package afox.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@ComponentScan("afox.club")
 public class SpringBootApp {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootApp.class, args);
